9ecf0904185aead78d721a268490357175f38667
guites <gui.garcia67@gmail.com>
2022-09-10 21:45:07 -0300

    add findWord command

---
+++ cmd/findWord.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2022 guites <gui.garcia67@gmail.com>
+
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"tweetsay/database"
+
+	"github.com/spf13/cobra"
+)
+
+// findWordCmd represents the findWord command
+var findWordCmd = &cobra.Command{
+	Use:   "findWord word",
+	Short: "Find tweets containing the given word",
+	Long: `Find tweets containing the given word amongst the tweets you have seen so far.`,
+	Args: func (cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("please include the word you want to search for")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		tweetExcerpts := database.FindTweetsContainingWord(args[0])
+		fmt.Printf("%v\n", tweetExcerpts)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(findWordCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// findWordCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// findWordCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- database/word.go
+++ database/word.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 	"strings"
 )
 
@@ -12,6 +14,7 @@ func AddWord (word string) int{
 	if db_err != nil {
 		log.Fatal("Error opening database", db_err)
 	}
+	defer db.Close()
 	
 	stmt, stmt_err := db.Prepare("INSERT INTO Words (word) VALUES (?)")
 	if stmt_err != nil {
@@ -44,6 +47,7 @@ func RelateWordToTweet(wordID int, tweetID int64) {
 	if db_err != nil {
 		log.Fatal("Error opening database", db_err)
 	}
+	defer db.Close()
 
 	stmt, stmt_err := db.Prepare("INSERT INTO WordAppearances (WordID, TweetID) VALUES (?, ?)")
 	if stmt_err != nil {
@@ -57,4 +61,51 @@ func RelateWordToTweet(wordID int, tweetID int64) {
 			log.Fatalf("Error while creating word - tweet relationship (%d, %d) in  database, details: %s", wordID, tweetID, err.Error())
 		}
 	}
+}
+
+// TODO: maybe define this in models package?
+type TweetExcerpt struct {
+	Username string
+	TweetID int64
+	TweetExcerpt string
+}
+
+// return a list of tweets containing the given word
+func FindTweetsContainingWord(word string) []TweetExcerpt {
+	db, db_err := sql.Open("sqlite3", getPath())
+	if db_err != nil {
+		log.Fatal("Error opening database", db_err)
+	}
+	defer db.Close()
+
+	var tweetExcerpts []TweetExcerpt
+	var tweetID int64
+	var tweetFullText string
+	var tweetUsername string
+	
+	rows, query_err := db.Query("SELECT t.id, t.FullText, t.Username FROM Tweets t INNER JOIN WordAppearances wa ON wa.TweetID = t.ID INNER JOIN Words w ON w.ID = wa.WordID WHERE w.word = (?) AND SoftDeleted = 0", word)
+	if query_err != nil {
+		log.Fatalf("Error while searching for tweets containing the word <<%s>>, detail: %s\n", word, query_err.Error())
+	}
+
+	for rows.Next() {
+		scan_err := rows.Scan(&tweetID, &tweetFullText, &tweetUsername)
+		if scan_err != nil {
+			log.Fatalln("Error while iterating over results from db:", scan_err)
+		}
+		tweetExcerpt := TweetExcerpt{
+			Username: tweetUsername,
+			TweetID: tweetID,
+			// TODO: include only a few words before and after the appearance of the searched word
+			TweetExcerpt: tweetFullText,
+		}
+		tweetExcerpts = append(tweetExcerpts, tweetExcerpt)
+	}
+
+	if len(tweetExcerpts) == 0 {
+		fmt.Printf("You haven't seen any tweet containing the word <<%s>> yet!\n", word)
+		os.Exit(0)
+	}
+
+	return tweetExcerpts
 }
